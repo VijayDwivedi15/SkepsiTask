8761e47997ce76e8a00c001e3b5f6796ba546ac9
vijaydwivedi125 <vijaydwivedi125@gmail.com>
2020-06-10 21:22:44 +0530

    Add project files.

---
+++ internshalaTask/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace internshalaTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static string resulttime = String.Empty;
+         
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult GetTimeData(float Sunrise = 0, float Sunset = 0)
+        {
+
+            var latval = Sunrise;
+            var longvar = Sunset;
+
+            resulttime = "https://api.sunrise-sunset.org/json?lat="+ Sunrise+"&lng="+Sunset+"&callback=mycallback";
+
+
+            var result = new { datares= resulttime };
+
+            
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+    }
+}
+}
---
+++ internshalaTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="internshalaTask.MvcApplication" Language="C#" %>
